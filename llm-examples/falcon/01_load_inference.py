4ec3b433d5842d36899b238b73384f203edb0fbf
es94129 <es94129>
2023-07-06 01:52:12 +0000

    Add Falcon README and edit inference example

--- llm-examples/falcon/01_load_inference.py
+++ llm-examples/falcon/01_load_inference.py
@@ -1,11 +1,19 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC # Falcon-7b-instruct Inference on Databricks
+# MAGIC [Falcon-7b-instruct](https://huggingface.co/tiiuae/falcon-7b-instruct) is a 7B parameters decoder-only model that is fine-tuned on chat/instruct datasets on top of [Falcon-7B](https://huggingface.co/tiiuae/falcon-7b), with context length 2048.
+# MAGIC
 # MAGIC Environment for this notebook:
 # MAGIC - Runtime: 13.1 GPU ML Runtime
 # MAGIC - Instance: `g5.4xlarge` on AWS
 # MAGIC
 # MAGIC GPU instances that have at least 16GB GPU memory would be enough for inference on single input (batch inference requires slightly more memory). On Azure, it is possible to use `Standard_NC6s_v3` or `Standard_NC4as_T4_v3`.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Install required packages
+# MAGIC Falcon uses PyTorch 2.0 and thus requires package upgrades.
 
 # COMMAND ----------
 
@@ -84,6 +92,7 @@ def gen_text(prompts, use_template=False, **kwargs):
     # configure other text generation arguments, see common configurable args here: https://huggingface.co/docs/transformers/main_classes/text_generation#transformers.GenerationConfig
     kwargs.update(
         {
+            "do_sample": True,  # by default when do_sample=False the generation method is greedy decoding; with do_sample=True, popular arguments such as temperature, top_p, and top_k could take effect
             "pad_token_id": tokenizer.eos_token_id,  # Hugging Face sets pad_token_id to eos_token_id by default; setting here to not see redundant message
             "eos_token_id": tokenizer.eos_token_id,
         }
@@ -108,6 +117,27 @@ print(results[0])
 
 # Use args such as temperature and max_new_tokens to control text generation
 results = gen_text(["What is a large language model?"], temperature=0.5, max_new_tokens=100, use_template=True)
+print(results[0])
+
+# COMMAND ----------
+
+# Check that the generation quality when the context is long
+
+long_input = """Provide a concise summary of the below passage.
+
+Hannah Arendt was one of the seminal political thinkers of the twentieth century. The power and originality of her thinking was evident in works such as The Origins of Totalitarianism, The Human Condition, On Revolution and The Life of the Mind. In these works and in numerous essays she grappled with the most crucial political events of her time, trying to grasp their meaning and historical import, and showing how they affected our categories of moral and political judgment. What was required, in her view, was a new framework that could enable us to come to terms with the twin horrors of the twentieth century, Nazism and Stalinism. She provided such framework in her book on totalitarianism, and went on to develop a new set of philosophical categories that could illuminate the human condition and provide a fresh perspective on the nature of political life.
+
+Although some of her works now belong to the classics of the Western tradition of political thought, she has always remained difficult to classify. Her political philosophy cannot be characterized in terms of the traditional categories of conservatism, liberalism, and socialism. Nor can her thinking be assimilated to the recent revival of communitarian political thought, to be found, for example, in the writings of A. MacIntyre, M. Sandel, C. Taylor and M. Walzer. Her name has been invoked by a number of critics of the liberal tradition, on the grounds that she presented a vision of politics that stood in opposition some key liberal principles. There are many strands of Arendt’s thought that could justify such a claim, in particular, her critique of representative democracy, her stress on civic engagement and political deliberation, her separation of morality from politics, and her praise of the revolutionary tradition. However, it would be a mistake to view Arendt as an anti-liberal thinker. Arendt was in fact a stern defender of constitutionalism and the rule of law, an advocate of fundamental human rights (among which she included not only the right to life, liberty, and freedom of expression, but also the right to action and to opinion), and a critic of all forms of political community based on traditional ties and customs, as well as those based on religious, ethnic, or racial identity.
+
+Arendt’s political thought cannot, in this sense, be identified either with the liberal tradition or with the claims advanced by a number of its critics. Arendt did not conceive of politics as a means for the satisfaction of individual preferences, nor as a way to integrate individuals around a shared conception of the good. Her conception of politics is based instead on the idea of active citizenship, that is, on the value and importance of civic engagement and collective deliberation about all matters affecting the political community. If there is a tradition of thought with which Arendt can be identified, it is the classical tradition of civic republicanism originating in Aristotle and embodied in the writings of Machiavelli, Montesquieu, Jefferson, and Tocqueville. According to this tradition politics finds its authentic expression whenever citizens gather together in a public space to deliberate and decide about matters of collective concern. Political activity is valued not because it may lead to agreement or to a shared conception of the good, but because it enables each citizen to exercise his or her powers of agency, to develop the capacities for judgment and to attain by concerted action some measure of political efficacy."""
+
+def get_num_tokens(text):
+    inputs = tokenizer(text, return_tensors="pt").input_ids.to("cuda")
+    return inputs.shape[1]
+
+print('number of tokens for input:', get_num_tokens(long_input))
+
+results = gen_text([long_input], max_new_tokens=200, use_template=True)
 print(results[0])
 
 # COMMAND ----------
@@ -147,3 +177,62 @@ for output in results:
   print('\n')
 
 # COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Measure inference speed
+# MAGIC Text generation speed is often measured with token/s, which is the average number of tokens that are generated by the model per second.
+
+# COMMAND ----------
+
+import time
+import logging
+
+
+def get_gen_text_time(prompt, use_template=True, **kwargs):
+    if use_template:
+        full_prompt = PROMPT_FOR_GENERATION_FORMAT.format(instruction=prompt)
+    else:
+        full_prompt = prompt
+
+    if "max_new_tokens" not in kwargs:
+        kwargs["max_new_tokens"] = 512
+
+    kwargs.update(
+        {
+            "do_sample": True,
+            "pad_token_id": tokenizer.eos_token_id,
+            "eos_token_id": tokenizer.eos_token_id,
+            "return_tensors": True,  # make the pipeline return token ids instead of decoded text to get the number of generated tokens
+        }
+    )
+
+    num_input_tokens = get_num_tokens(full_prompt)
+
+    # measure the time it takes for text generation
+    start = time.time()
+    outputs = pipeline(full_prompt, **kwargs)
+    duration = time.time() - start
+
+    # get the number of generated tokens
+    n_tokens = len(outputs[0]["generated_token_ids"])
+    
+    logging.warning(
+        f"{n_tokens/duration} tokens/sec, {n_tokens} tokens (including full prompt)"
+    )
+
+    result = tokenizer.batch_decode(
+        outputs[0]["generated_token_ids"][num_input_tokens:], skip_special_tokens=True
+    )
+
+    return "".join(result)
+
+# COMMAND ----------
+
+result = get_gen_text_time("What is ML?", max_new_tokens=200, use_template=True)
+print(result)
+
+# COMMAND ----------
+
+# When the context is long or the generated text is long, it takes longer to generate each token
+result = get_gen_text_time(long_input, max_new_tokens=200, use_template=True)
+print(result)
