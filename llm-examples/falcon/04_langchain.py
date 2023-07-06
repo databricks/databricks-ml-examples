5c551b7a936ee63decf29255861a599e0647b50d
es94129 <es94129>
2023-07-06 18:20:25 +0000

    Add langchain example notebook

---
+++ llm-examples/falcon/04_langchain.py
@@ -0,0 +1,90 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Load Falcon-7B-Instruct from LangChain on Databricks
+# MAGIC
+# MAGIC This example notebook is adapts the [LangChain integration documentation](https://python.langchain.com/docs/modules/model_io/models/llms/integrations/databricks), and shows how to wrap Databricks endpoints as LLMs in LangChain.
+# MAGIC
+# MAGIC Requirements:
+# MAGIC - Keep the driver proxy app of the `03_serve_driver_proxy` example notebook running.
+# MAGIC - MLR: 13.1 ML+
+# MAGIC - Instance: No actual requirements
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Same cluster
+# MAGIC If using the same cluster that runs the `03_serve_driver_proxy` notebook, specifying `cluster_driver_port` is required.
+
+# COMMAND ----------
+
+# MAGIC %pip install -q -U langchain
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from langchain.llms import Databricks
+
+# COMMAND ----------
+
+llm = Databricks(cluster_driver_port="7777")
+
+print(llm("How to master Python in 3 days?"))
+
+# COMMAND ----------
+
+# If the app accepts extra parameters like `temperature`,
+# you can set them in `model_kwargs`.
+llm = Databricks(cluster_driver_port="7777", model_kwargs={"temperature": 0.1})
+
+print(llm("How to master Python in 3 days?"))
+
+# COMMAND ----------
+
+# Use `transform_input_fn` and `transform_output_fn` if the app
+# expects a different input schema and does not return a JSON string,
+# respectively, or you want to apply a prompt template on top.
+
+
+def transform_input(**request):
+    full_prompt = f"""{request["prompt"]}
+    Be Concise.
+    """
+    request["prompt"] = full_prompt
+    return request
+
+
+def transform_output(response):
+    return response.upper()
+
+
+llm = Databricks(
+    cluster_driver_port="7777",
+    transform_input_fn=transform_input,
+    transform_output_fn=transform_output,
+)
+
+print(llm("How to master Python in 3 days?"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Different cluster
+# MAGIC If using a different cluster, it's required to also specify `cluster_id`, which you can find in the cluster configuration page.
+
+# COMMAND ----------
+
+# MAGIC %pip install -q -U langchain
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from langchain.llms import Databricks
+
+# TODO: this cluster ID is a placeholder, please replace `cluster_id` with the actual cluster ID of the server proxy app's cluster
+llm = Databricks(cluster_id="0621-205218-lzbwa3m8", cluster_driver_port="7777")
+
+print(llm("How to master Python in 3 days?"))
+
+# COMMAND ----------
+
+
--- llm-examples/falcon/README.md
+++ llm-examples/falcon/README.md
@@ -23,8 +23,7 @@ This folder contains the following examples for Falcon models:
 | `01_load_inference`  | Environment setup and suggested configurations when using Falcon models for inference on Databricks. | `falcon-7b-instruct`  | 16GB |
 | `02_mlflow_logging_inference` | Save, register, and load Falcon models with MLflow. | `falcon-7b-instruct`  | 16GB |
 | `03_serve_driver_proxy` | Serve Falcon models on the cluster driver node with Flask.  | `falcon-7b-instruct` | 16GB |
+| `04_langchain` | Wrap an endpoint created by `03_serve_driver_proxy` with LangChain and query it. | N/A | N/A |
 <!---
-| `04_serve_endpoint` | Serve Falcon models with a Databricks serving endpoint.  | `falcon-7b-instruct` | 16GB |
-| `05_langchain` | Wrap an endpoint created by `03_serve_driver_proxy` or `04_serve_endpoint` with LangChain and query it. | N/A | N/A |
-| `06_fine_tune_deepspeed` | Fine-tune Falcon base models with DeepSpeed. | `falcon-7b` | 160GB |
+| `05_fine_tune_deepspeed` | Fine-tune Falcon base models with DeepSpeed. | `falcon-7b` | 160GB |
 -->
