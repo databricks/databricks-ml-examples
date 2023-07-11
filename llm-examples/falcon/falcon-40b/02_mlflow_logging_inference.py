021954ee75b9ac2804b7c871320f461214213372
es94129 <es94129>
2023-07-11 21:14:09 +0000

    [wip] Add falcon 40b mlflow

---
+++ llm-examples/falcon/falcon-40b/02_mlflow_logging_inference.py
@@ -0,0 +1,256 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Manage Falcon-40b-instruct model with MLFlow on Databricks
+# MAGIC Environment for this notebook:
+# MAGIC - Runtime: 13.1 GPU ML Runtime
+# MAGIC - Instance: `g5.48xlarge` on AWS required when loading the model for inference
+# MAGIC
+# MAGIC On Azure, we suggest using `Standard_NC48ads_A100_v4` (2 A100-80GB GPUs).
+
+# COMMAND ----------
+
+# MAGIC %pip install -q -U torch==2.0.1
+# MAGIC %pip install -q einops==0.6.1
+# MAGIC %pip install -q -U mlflow-skinny[databricks]==2.4.1
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Log the model
+
+# COMMAND ----------
+
+from huggingface_hub import snapshot_download
+
+# If the model has been downloaded in previous cells, this will not repetitively download large model files, but only the remaining files in the repo
+snapshot_location = snapshot_download(
+    repo_id="tiiuae/falcon-40b-instruct",
+    cache_dir="/local_disk0/.cache/huggingface/",
+    ignore_patterns="coreml/*",
+    revision="1e7fdcc9f45d13704f3826e99937917e007cd975",
+)
+
+# COMMAND ----------
+
+import mlflow
+import torch
+import transformers
+
+# Define PythonModel to log with mlflow.pyfunc.log_model
+
+class Falcon(mlflow.pyfunc.PythonModel):
+    def load_context(self, context):
+        """
+        This method initializes the tokenizer and language model
+        using the specified model repository.
+        """
+        # Initialize tokenizer and language model
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            context.artifacts['repository'], padding_side="left")
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(
+            context.artifacts['repository'], 
+            torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True, 
+            trust_remote_code=True,
+            device_map="auto",
+            pad_token_id=self.tokenizer.eos_token_id).to('cuda')
+        self.model.eval()
+
+    def _build_prompt(self, instruction):
+        """
+        This method generates the prompt for the model.
+        """
+        INSTRUCTION_KEY = "### Instruction:"
+        RESPONSE_KEY = "### Response:"
+        INTRO_BLURB = (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request."
+        )
+
+        return f"""{INTRO_BLURB}
+        {INSTRUCTION_KEY}
+        {instruction}
+        {RESPONSE_KEY}
+        """
+
+    def _generate_response(self, prompt, temperature, max_new_tokens):
+        """
+        This method generates prediction for a single input.
+        """
+        # Build the prompt
+        prompt = self._build_prompt(prompt)
+
+        # Encode the input and generate prediction
+        encoded_input = self.tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+        output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_new_tokens=max_new_tokens)
+    
+        # Decode the prediction to text
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+        # Removing the prompt from the generated text
+        prompt_length = len(self.tokenizer.encode(prompt, return_tensors='pt')[0])
+        generated_response = self.tokenizer.decode(output[0][prompt_length:], skip_special_tokens=True)
+
+        return generated_response
+      
+    def predict(self, context, model_input):
+        """
+        This method generates prediction for the given input.
+        """
+
+        outputs = []
+
+        for i in range(len(model_input)):
+          prompt = model_input["prompt"][i]
+          temperature = model_input.get("temperature", [1.0])[i]
+          max_new_tokens = model_input.get("max_new_tokens", [100])[i]
+
+          outputs.append(self._generate_response(prompt, temperature, max_new_tokens))
+      
+        return outputs
+
+# COMMAND ----------
+
+from mlflow.models.signature import ModelSignature
+from mlflow.types import DataType, Schema, ColSpec
+
+import pandas as pd
+
+# Define input and output schema
+input_schema = Schema([
+    ColSpec(DataType.string, "prompt"), 
+    ColSpec(DataType.double, "temperature"), 
+    ColSpec(DataType.long, "max_new_tokens")])
+output_schema = Schema([ColSpec(DataType.string)])
+signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+
+# Define input example
+input_example=pd.DataFrame({
+            "prompt":["what is ML?"], 
+            "temperature": [0.5],
+            "max_new_tokens": [100]})
+
+# Log the model with its details such as artifacts, pip requirements and input example
+# This may take about 12 minutes to complete
+with mlflow.start_run() as run:  
+    mlflow.pyfunc.log_model(
+        "model",
+        python_model=Falcon(),
+        artifacts={'repository' : snapshot_location},
+        pip_requirements=["torch", "transformers", "accelerate", "einops","sentencepiece"],
+        input_example=input_example,
+        signature=signature,
+    )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Register the model to Unity Catalog
+
+# COMMAND ----------
+
+# Configure MLflow Python client to register model in Unity Catalog
+import mlflow
+mlflow.set_registry_uri("databricks-uc")
+
+# COMMAND ----------
+
+# Register model in Unity Catalog
+# This may take 20 minutes to complete
+result = mlflow.register_model(
+    "runs:/"+run.info.run_id+"/model",
+    "prod.ml_team.falcon_40b_instruct_model", # Note that the UC model name follows the pattern <catalog_name>.<schema_name>.<model_name>, corresponding to  the catalog, schema, and registered model name
+)
+
+# COMMAND ----------
+
+# Mark the registered model with alias "Champion"
+from mlflow import MlflowClient
+client = MlflowClient()
+client.set_registered_model_alias("prod.ml_team.falcon_40b_instruct_model", "Champion", 1)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Load the model from Unity Catalog
+
+# COMMAND ----------
+
+import mlflow
+
+mlflow.set_registry_uri("databricks-uc")
+loaded_model = mlflow.pyfunc.load_model("models:/prod.ml_team.falcon_40b_instruct_model@Champion")
+
+# Make a prediction using the loaded model
+loaded_model.predict(
+    {
+        "prompt": ["What is ML?", "What is large language model?"],
+        "temperature": [0.1, 0.5],
+        "max_new_tokens": [100, 100],
+    }
+)
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC df -h
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Create Model Serving Endpoint
+# MAGIC Once the model is registered, we can use API to create a Databricks GPU Model Serving Endpoint that serves the Falcon-40B-Instruct model.
+
+# COMMAND ----------
+
+# Provide a name to the serving endpoint
+endpoint_name = 'falcon-40b-instruct-example'
+
+# COMMAND ----------
+
+databricks_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+
+# COMMAND ----------
+
+import requests
+import json
+
+deploy_headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+deploy_url = f'{databricks_url}/api/2.0/serving-endpoints'
+
+model_version = result  # the returned result of mlflow.register_model
+endpoint_config = {
+  "name": endpoint_name,
+  "config": {
+    "served_models": [{
+      "name": f'{model_version.name.replace(".", "_")}_{model_version.version}',
+      "model_name": model_version.name,
+      "model_version": model_version.version,
+      "workload_type": "GPU_MEDIUM",
+      "workload_size": "Small",
+      "scale_to_zero_enabled": "False"
+    }]
+  }
+}
+endpoint_json = json.dumps(endpoint_config, indent='  ')
+
+# Send a POST request to the API
+deploy_response = requests.request(method='POST', headers=deploy_headers, url=deploy_url, data=endpoint_json)
+
+if deploy_response.status_code != 200:
+  raise Exception(f'Request failed with status {deploy_response.status_code}, {deploy_response.text}')
+
+# Show the response of the POST request
+# When first creating the serving endpoint, it should show that the state 'ready' is 'NOT_READY'
+# You can check the status on the Databricks model serving endpoint page, it is expected to take ~35 min for the serving endpoint to become ready
+print(deploy_response.json())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Once the model serving endpoint is ready, you can query it easily with LangChain (see `04_langchain` for example code) running in the same workspace.
