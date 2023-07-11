bb0a5081239c091ec3083e4105364bfce3202f72
es94129 <es94129>
2023-07-11 20:49:09 +0000

    Add Falcon UC example

--- llm-examples/falcon/falcon-7b/02_mlflow_logging_inference.py
+++ llm-examples/falcon/falcon-7b/02_mlflow_logging_inference.py
@@ -11,6 +11,7 @@
 
 # MAGIC %pip install -q -U torch==2.0.1
 # MAGIC %pip install -q einops==0.6.1
+# MAGIC %pip install -q -U mlflow-skinny[databricks]==2.4.1
 
 # COMMAND ----------
 
@@ -139,29 +140,47 @@ with mlflow.start_run() as run:
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Register the model
+# MAGIC ## Register the model to Unity Catalog
+# MAGIC By default, MLflow registers models in the Databricks workspace model registry. To register models in Unity Catalog instead, we follow the [documentation](https://docs.databricks.com/machine-learning/manage-model-lifecycle/index.html) and set the registry server as Databricks Unity Catalog.
+# MAGIC
+# MAGIC In order to register a model in Unity Catalog, there are [several requirements](https://docs.databricks.com/machine-learning/manage-model-lifecycle/index.html#requirements), such as Unity Catalog must be enabled in your workspace.
 
 # COMMAND ----------
 
-# Register model in MLflow Model Registry
-# This may take 7 minutes to complete
+# Configure MLflow Python client to register model in Unity Catalog
+import mlflow
+mlflow.set_registry_uri("databricks-uc")
+
+# COMMAND ----------
+
+# Register model to Unity Catalog
+# This may take 5 minutes to complete
+
+registered_name = "models.default.falcon_7b_instruct_model", # Note that the UC model name follows the pattern <catalog_name>.<schema_name>.<model_name>, corresponding to the catalog, schema, and registered model name
+
 result = mlflow.register_model(
     "runs:/"+run.info.run_id+"/model",
-    name="falcon-7b-instruct-demo",
-    await_registration_for=600,
+    registered_name,
 )
 
 # COMMAND ----------
 
+# Mark model for deployment using an alias
+from mlflow import MlflowClient
+client = MlflowClient()
+client.set_registered_model_alias(registered_name, "Champion", 1)
+
+# COMMAND ----------
+
 # MAGIC %md
-# MAGIC ## Load the model from model registry
+# MAGIC ## Load the model from Unity Catalog
 
 # COMMAND ----------
 
 import mlflow
 import pandas as pd
 
-loaded_model = mlflow.pyfunc.load_model(f"models:/falcon-7b-instruct-demo/latest")
+loaded_model = mlflow.pyfunc.load_model(f"models:/{registered_name}@Champion")
 
 # Make a prediction using the loaded model
 loaded_model.predict(
