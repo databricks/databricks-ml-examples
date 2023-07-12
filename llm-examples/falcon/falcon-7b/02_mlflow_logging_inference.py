1b88083dfba5e0a5ee9b22c335f52209d62668eb
Ying Chen <ying.chen@databricks.com>
2023-07-12 16:45:49 -0700

    Fix bug

--- llm-examples/falcon/falcon-7b/02_mlflow_logging_inference.py
+++ llm-examples/falcon/falcon-7b/02_mlflow_logging_inference.py
@@ -156,7 +156,7 @@ mlflow.set_registry_uri("databricks-uc")
 # Register model to Unity Catalog
 # This may take 5 minutes to complete
 
-registered_name = "models.default.falcon_7b_instruct_model", # Note that the UC model name follows the pattern <catalog_name>.<schema_name>.<model_name>, corresponding to the catalog, schema, and registered model name
+registered_name = "models.default.falcon_7b_instruct_model" # Note that the UC model name follows the pattern <catalog_name>.<schema_name>.<model_name>, corresponding to the catalog, schema, and registered model name
 
 result = mlflow.register_model(
     "runs:/"+run.info.run_id+"/model",
