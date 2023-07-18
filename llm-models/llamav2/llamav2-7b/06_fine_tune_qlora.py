342700694e60a9100e8bd611620c53cfb98cfcd1
lu-wang-dl <lu-wang-dl>
2023-07-18 17:30:13 +0000

    Add llama-2 example

---
+++ llm-models/llamav2/llamav2-7b/06_fine_tune_qlora.py
@@ -0,0 +1,327 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Fine tune llama-2-7b-hf with QLORA
+# MAGIC
+# MAGIC This is to fine-tune [llama-2-7b-hf](https://huggingface.co/meta-llama/Llama-2-7b-hf) models on the [databricks-dolly-15k](https://huggingface.co/datasets/databricks/databricks-dolly-15k) dataset.
+# MAGIC
+# MAGIC Environment for this notebook:
+# MAGIC - Runtime: 13.2 GPU ML Runtime
+# MAGIC - Instance: `g5.4xlarge` on AWS
+# MAGIC
+# MAGIC We will leverage PEFT library from Hugging Face ecosystem, as well as QLoRA for more memory efficient finetuning
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Install required packages
+# MAGIC
+# MAGIC Run the cells below to setup and install the required libraries. For our experiment we will need `accelerate`, `peft`, `transformers`, `datasets` and TRL to leverage the recent [`SFTTrainer`](https://huggingface.co/docs/trl/main/en/sft_trainer). We will use `bitsandbytes` to [quantize the base model into 4bit](https://huggingface.co/blog/4bit-transformers-bitsandbytes). We will also install `einops` as it is a requirement to load Falcon models.
+
+# COMMAND ----------
+
+# MAGIC %pip install -U git+https://github.com/huggingface/transformers.git  git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git
+# MAGIC %pip install datasets==2.12.0 bitsandbytes==0.40.1 einops==0.6.1 trl==0.4.7
+# MAGIC %pip install torch==2.0.1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Dataset
+# MAGIC
+# MAGIC We will use the [databricks-dolly-15k ](https://huggingface.co/datasets/databricks/databricks-dolly-15k) dataset.
+
+# COMMAND ----------
+
+from datasets import load_dataset
+
+dataset_name = "databricks/databricks-dolly-15k"
+dataset = load_dataset(dataset_name, split="train")
+
+# COMMAND ----------
+
+INTRO_BLURB = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+INSTRUCTION_KEY = "### Instruction:"
+INPUT_KEY = "Input:"
+RESPONSE_KEY = "### Response:"
+END_KEY = "### End"
+
+PROMPT_NO_INPUT_FORMAT = """{intro}
+
+{instruction_key}
+{instruction}
+
+{response_key}
+{response}
+
+{end_key}""".format(
+  intro=INTRO_BLURB,
+  instruction_key=INSTRUCTION_KEY,
+  instruction="{instruction}",
+  response_key=RESPONSE_KEY,
+  response="{response}",
+  end_key=END_KEY
+)
+
+PROMPT_WITH_INPUT_FORMAT = """{intro}
+
+{instruction_key}
+{instruction}
+
+{input_key}
+{input}
+
+{response_key}
+{response}
+
+{end_key}""".format(
+  intro=INTRO_BLURB,
+  instruction_key=INSTRUCTION_KEY,
+  instruction="{instruction}",
+  input_key=INPUT_KEY,
+  input="{input}",
+  response_key=RESPONSE_KEY,
+  response="{response}",
+  end_key=END_KEY
+)
+
+def apply_prompt_template(examples):
+  instruction = examples["instruction"]
+  response = examples["response"]
+  context = examples.get("context")
+
+  if context:
+    full_prompt = PROMPT_WITH_INPUT_FORMAT.format(instruction=instruction, response=response, input=context)
+  else:
+    full_prompt = PROMPT_NO_INPUT_FORMAT.format(instruction=instruction, response=response)
+  return { "text": full_prompt }
+
+dataset = dataset.map(apply_prompt_template)
+
+# COMMAND ----------
+
+dataset["text"][0]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Loading the model
+# MAGIC
+# MAGIC In this section we will load the [LLaMAV2](), quantize it in 4bit and attach LoRA adapters on it.
+
+# COMMAND ----------
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoTokenizer
+
+model = "elinas/llama-7b-hf-transformers-4.29"
+revision = "d33594ee64ef1b6264543b6a88f60982a55fdb7a"
+
+tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model,
+    quantization_config=bnb_config,
+    device_map="auto",
+    revision=revision,
+    trust_remote_code=True
+)
+model.config.use_cache = False
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Load the configuration file in order to create the LoRA model. 
+# MAGIC
+# MAGIC According to QLoRA paper, it is important to consider all linear layers in the transformer block for maximum performance. Therefore we will add `dense`, `dense_h_to_4_h` and `dense_4h_to_h` layers in the target modules in addition to the mixed query key value layer.
+
+# COMMAND ----------
+
+import bitsandbytes as bnb
+
+def find_all_linear_names(model):
+    cls = bnb.nn.Linear4bit # if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+
+    if 'lm_head' in lora_module_names: # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+# COMMAND ----------
+
+find_all_linear_names(model)
+
+# COMMAND ----------
+
+from peft import LoraConfig
+
+lora_alpha = 16
+lora_dropout = 0.1
+lora_r = 64
+
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=['q_proj', 'o_proj', 'gate_proj', 'up_proj', 'down_proj', 'k_proj', 'v_proj']
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Loading the trainer
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Here we will use the [`SFTTrainer` from TRL library](https://huggingface.co/docs/trl/main/en/sft_trainer) that gives a wrapper around transformers `Trainer` to easily fine-tune models on instruction based datasets using PEFT adapters. Let's first load the training arguments below.
+
+# COMMAND ----------
+
+from transformers import TrainingArguments
+
+output_dir = "/dbfs/results"
+per_device_train_batch_size = 16
+gradient_accumulation_steps = 4
+optim = "paged_adamw_32bit"
+save_steps = 10
+logging_steps = 10
+learning_rate = 2e-4
+max_grad_norm = 0.3
+max_steps = 500
+warmup_ratio = 0.03
+lr_scheduler_type = "constant"
+
+training_arguments = TrainingArguments(
+    output_dir=output_dir,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    optim=optim,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+    learning_rate=learning_rate,
+    fp16=True,
+    max_grad_norm=max_grad_norm,
+    max_steps=max_steps,
+    warmup_ratio=warmup_ratio,
+    group_by_length=True,
+    lr_scheduler_type=lr_scheduler_type,
+    ddp_find_unused_parameters=False,
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Then finally pass everthing to the trainer
+
+# COMMAND ----------
+
+from trl import SFTTrainer
+
+max_seq_length = 512
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset,
+    peft_config=peft_config,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    tokenizer=tokenizer,
+    args=training_arguments,
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC We will also pre-process the model by upcasting the layer norms in float 32 for more stable training
+
+# COMMAND ----------
+
+for name, module in trainer.model.named_modules():
+    if "norm" in name:
+        module = module.to(torch.float32)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Train the model
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Now let's train the model! Simply call `trainer.train()`
+
+# COMMAND ----------
+
+trainer.train()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Save the LORA model
+
+# COMMAND ----------
+
+model.save_pretrained("/local_disk0/llamav2-7b-lora-fine-tune")
+
+# COMMAND ----------
+
+import torch
+from peft import PeftModel, PeftConfig
+
+peft_model_id = "/local_disk0/llamav2-7b-lora-fine-tune"
+config = PeftConfig.from_pretrained(peft_model_id)
+
+from huggingface_hub import snapshot_download
+# Download the MPT model snapshot from huggingface
+snapshot_location = snapshot_download(repo_id=config.base_model_name_or_path)
+
+
+# COMMAND ----------
+
+import mlflow
+class LLAMAQLORA(mlflow.pyfunc.PythonModel):
+  def load_context(self, context):
+    self.tokenizer = AutoTokenizer.from_pretrained(context.artifacts['repository'])
+    self.tokenizer.pad_token = tokenizer.eos_token
+    config = PeftConfig.from_pretrained(context.artifacts['lora'])
+    base_model = AutoModelForCausalLM.from_pretrained(
+      context.artifacts['repository'], 
+      return_dict=True, 
+      load_in_4bit=True, 
+      device_map={"":0},
+      trust_remote_code=True,
+    )
+    self.model = PeftModel.from_pretrained(base_model, peft_model_id)
+  
+  def predict(self, context, model_input):
+    prompt = model_input["prompt"][0]
+    temperature = model_input.get("temperature", [1.0])[0]
+    max_tokens = model_input.get("max_tokens", [100])[0]
+    batch = self.tokenizer(prompt, padding=True, truncation=True,return_tensors='pt').to('cuda')
+    with torch.cuda.amp.autocast():
+      output_tokens = self.model.generate(
+          input_ids = batch.input_ids, 
+          max_new_tokens=max_tokens,
+          temperature=temperature,
+          top_p=0.7,
+          num_return_sequences=1,
+          pad_token_id=tokenizer.eos_token_id,
+          eos_token_id=tokenizer.eos_token_id,
+      )
+    generated_text = self.tokenizer.decode(output_tokens[0], skip_special_tokens=True)
+
+    return generated_text
