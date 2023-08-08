b1ca47c058461f7fde214914d53b051990064d94
Lu Wang <38018689+lu-wang-dl@users.noreply.github.com>
2023-08-08 11:08:50 -0700

    Add do_sample

--- llm-models/llamav2/llamav2-7b/06_fine_tune_qlora.py
+++ llm-models/llamav2/llamav2-7b/06_fine_tune_qlora.py
@@ -311,6 +311,7 @@ class LLAMAQLORA(mlflow.pyfunc.PythonModel):
           temperature=temperature,
           top_p=0.7,
           num_return_sequences=1,
+          do_sample=True,
           pad_token_id=tokenizer.eos_token_id,
           eos_token_id=tokenizer.eos_token_id,
       )
