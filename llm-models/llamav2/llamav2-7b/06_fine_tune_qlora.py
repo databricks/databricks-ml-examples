aaa322bcbaac298acd6fed7a499f29e8639cc233
Ying Chen <ying.chen@databricks.com>
2023-08-10 15:00:46 -0700

    Update 06_fine_tune_qlora.py

--- llm-models/llamav2/llamav2-7b/06_fine_tune_qlora.py
+++ llm-models/llamav2/llamav2-7b/06_fine_tune_qlora.py
@@ -28,9 +28,9 @@ notebook_login()
 
 # COMMAND ----------
 
-# MAGIC %pip install -U git+https://github.com/huggingface/transformers@v4.31-release git+https://github.com/huggingface/peft.git # git+https://github.com/huggingface/accelerate.git
+# MAGIC %pip install git+https://github.com/huggingface/peft.git
 # MAGIC %pip install datasets==2.12.0 bitsandbytes==0.40.1 einops==0.6.1 trl==0.4.7
-# MAGIC %pip install torch==2.0.1 accelerate==0.21.0
+# MAGIC %pip install torch==2.0.1 accelerate==0.21.0 transformers==4.31.0
 
 # COMMAND ----------
 
