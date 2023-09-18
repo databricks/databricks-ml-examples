13c5391df0912acd90a87be60f40e6afaa9ea887
lu-wang-dl <lu.wang@databricks.com>
2023-09-18 01:15:44 -0700

    add origin

---
+++ llm-models/llamav2/llamav2-7b/02_mlflow_logging_inference.py
@@ -0,0 +1,264 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Manage Llama 2 7B chat model with MLFlow on Databricks
+# MAGIC
+# MAGIC [Llama 2](https://huggingface.co/meta-llama) is a collection of pretrained and fine-tuned generative text models ranging in scale from 7 billion to 70 billion parameters. It is trained with 2T tokens and supports context length window upto 4K tokens. [Llama-2-7b-chat-hf](https://huggingface.co/meta-llama/Llama-2-7b-chat-hf) is the 7B fine-tuned model, optimized for dialogue use cases and converted for the Hugging Face Transformers format.
+# MAGIC
+# MAGIC Environment for this notebook:
+# MAGIC - Runtime: 13.2 GPU ML Runtime
+# MAGIC - Instance: `g5.4xlarge` on AWS, `Standard_NV36ads_A10_v5` on Azure
+# MAGIC
+# MAGIC Requirements:
+# MAGIC - To get the access of the model on HuggingFace, please visit the [Meta website](https://ai.meta.com/resources/models-and-libraries/llama-downloads) and accept our license terms and acceptable use policy before submitting this form. Requests will be processed in 1-2 days.
+
+# COMMAND ----------
+
+# MAGIC %pip install --upgrade "mlflow-skinny[databricks]>=2.4.1"
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+from huggingface_hub import notebook_login
+
+# Login to Huggingface to get access to the model
+notebook_login()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Log the model to MLFlow
+
+# COMMAND ----------
+
+# it is suggested to pin the revision commit hash and not change it for reproducibility because the uploader might change the model afterwards; you can find the commmit history of llamav2-7b-chat in https://huggingface.co/meta-llama/Llama-2-7b-chat-hf/commits/main
+model = "meta-llama/Llama-2-7b-chat-hf"
+revision = "0ede8dd71e923db6258295621d817ca8714516d4"
+
+from huggingface_hub import snapshot_download
+
+# If the model has been downloaded in previous cells, this will not repetitively download large model files, but only the remaining files in the repo
+snapshot_location = snapshot_download(repo_id=model, revision=revision, ignore_patterns="*.safetensors*")
+
+# COMMAND ----------
+
+import mlflow
+import torch
+import transformers
+
+# Define prompt template to get the expected features and performance for the chat versions. See our reference code in github for details: https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L212
+
+DEFAULT_SYSTEM_PROMPT = """\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+
+# Define PythonModel to log with mlflow.pyfunc.log_model
+
+class Llama2(mlflow.pyfunc.PythonModel):
+    def load_context(self, context):
+        """
+        This method initializes the tokenizer and language model
+        using the specified model repository.
+        """
+        # Initialize tokenizer and language model
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            context.artifacts['repository'], padding_side="left")
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(
+            context.artifacts['repository'], 
+            torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True, 
+            trust_remote_code=True,
+            device_map="auto",
+            pad_token_id=self.tokenizer.eos_token_id)
+        self.model.eval()
+
+    def _build_prompt(self, instruction):
+        """
+        This method generates the prompt for the model.
+        """
+        return f"""<s>[INST]<<SYS>>\n{DEFAULT_SYSTEM_PROMPT}\n<</SYS>>\n\n\n{instruction}[/INST]\n"""
+
+    def _generate_response(self, prompt, temperature, max_new_tokens):
+        """
+        This method generates prediction for a single input.
+        """
+        # Build the prompt
+        prompt = self._build_prompt(prompt)
+
+        # Encode the input and generate prediction
+        encoded_input = self.tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+        output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_new_tokens=max_new_tokens)
+    
+        # Decode the prediction to text
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+        # Removing the prompt from the generated text
+        prompt_length = len(self.tokenizer.encode(prompt, return_tensors='pt')[0])
+        generated_response = self.tokenizer.decode(output[0][prompt_length:], skip_special_tokens=True)
+
+        return generated_response
+      
+    def predict(self, context, model_input):
+        """
+        This method generates prediction for the given input.
+        """
+
+        outputs = []
+
+        for i in range(len(model_input)):
+          prompt = model_input["prompt"][i]
+          temperature = model_input.get("temperature", [1.0])[i]
+          max_new_tokens = model_input.get("max_new_tokens", [100])[i]
+
+          outputs.append(self._generate_response(prompt, temperature, max_new_tokens))
+      
+        # {"candidates": [...]} is the required response format for MLflow AI gateway -- see 07_ai_gateway for example
+        return {"candidates": outputs}
+
+# COMMAND ----------
+
+from mlflow.models.signature import ModelSignature
+from mlflow.types import DataType, Schema, ColSpec
+
+import pandas as pd
+
+# Define input and output schema
+input_schema = Schema([
+    ColSpec(DataType.string, "prompt"), 
+    ColSpec(DataType.double, "temperature"), 
+    ColSpec(DataType.long, "max_new_tokens")])
+output_schema = Schema([ColSpec(DataType.string)])
+signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+
+# Define input example
+input_example=pd.DataFrame({
+            "prompt":["what is ML?"], 
+            "temperature": [0.5],
+            "max_new_tokens": [100]})
+
+# Log the model with its details such as artifacts, pip requirements and input example
+# This may take about 1.7 minutes to complete
+with mlflow.start_run() as run:  
+    mlflow.pyfunc.log_model(
+        "model",
+        python_model=Llama2(),
+        artifacts={'repository' : snapshot_location},
+        pip_requirements=["torch", "transformers", "accelerate", "sentencepiece"],
+        input_example=input_example,
+        signature=signature,
+    )
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Register the model to Unity Catalog
+# MAGIC  By default, MLflow registers models in the Databricks workspace model registry. To register models in Unity Catalog instead, we follow the [documentation](https://docs.databricks.com/machine-learning/manage-model-lifecycle/index.html) and set the registry server as Databricks Unity Catalog.
+# MAGIC
+# MAGIC  In order to register a model in Unity Catalog, there are [several requirements](https://docs.databricks.com/machine-learning/manage-model-lifecycle/index.html#requirements), such as Unity Catalog must be enabled in your workspace.
+# MAGIC
+
+# COMMAND ----------
+
+# Configure MLflow Python client to register model in Unity Catalog
+import mlflow
+mlflow.set_registry_uri("databricks-uc")
+
+# COMMAND ----------
+
+# Register model to Unity Catalog
+# This may take 2.2 minutes to complete
+
+registered_name = "models.default.llamav2_7b_chat_model" # Note that the UC model name follows the pattern <catalog_name>.<schema_name>.<model_name>, corresponding to the catalog, schema, and registered model name
+
+
+result = mlflow.register_model(
+    "runs:/"+run.info.run_id+"/model",
+    registered_name,
+)
+
+# COMMAND ----------
+
+from mlflow import MlflowClient
+client = MlflowClient()
+
+# Choose the right model version registered in the above cell.
+client.set_registered_model_alias(name=registered_name, alias="Champion", version=result.version)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Load the model from Unity Catalog
+
+# COMMAND ----------
+
+import mlflow
+import pandas as pd
+
+loaded_model = mlflow.pyfunc.load_model(f"models:/{registered_name}@Champion")
+
+# Make a prediction using the loaded model
+loaded_model.predict(
+    {
+        "prompt": ["What is ML?", "What is large language model?"],
+        "temperature": [0.1, 0.5],
+        "max_new_tokens": [100, 100],
+    }
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Create Model Serving Endpoint
+# MAGIC Once the model is registered, we can use API to create a Databricks GPU Model Serving Endpoint that serves the `LLaMAV2-7b` model.
+# MAGIC
+# MAGIC Note that the below deployment requires GPU model serving. For more information on GPU model serving, contact the Databricks team or sign up [here](https://docs.google.com/forms/d/1-GWIlfjlIaclqDz6BPODI2j1Xg4f4WbFvBXyebBpN-Y/edit).
+
+# COMMAND ----------
+
+# Provide a name to the serving endpoint
+endpoint_name = 'llama2-7b-chat'
+
+# COMMAND ----------
+
+databricks_url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().getOrElse(None)
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().getOrElse(None)
+
+# COMMAND ----------
+
+import requests
+import json
+
+deploy_headers = {'Authorization': f'Bearer {token}', 'Content-Type': 'application/json'}
+deploy_url = f'{databricks_url}/api/2.0/serving-endpoints'
+
+model_version = result  # the returned result of mlflow.register_model
+endpoint_config = {
+  "name": endpoint_name,
+  "config": {
+    "served_models": [{
+      "name": f'{model_version.name.replace(".", "_")}_{model_version.version}',
+      "model_name": model_version.name,
+      "model_version": model_version.version,
+      "workload_type": "GPU_MEDIUM",
+      "workload_size": "Small",
+      "scale_to_zero_enabled": "False"
+    }]
+  }
+}
+endpoint_json = json.dumps(endpoint_config, indent='  ')
+
+# Send a POST request to the API
+deploy_response = requests.request(method='POST', headers=deploy_headers, url=deploy_url, data=endpoint_json)
+
+if deploy_response.status_code != 200:
+  raise Exception(f'Request failed with status {deploy_response.status_code}, {deploy_response.text}')
+
+# Show the response of the POST request
+# When first creating the serving endpoint, it should show that the state 'ready' is 'NOT_READY'
+# You can check the status on the Databricks model serving endpoint page, it is expected to take ~35 min for the serving endpoint to become ready
+print(deploy_response.json())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Once the model serving endpoint is ready, you can query it easily with LangChain (see `04_langchain` for example code) running in the same workspace.
