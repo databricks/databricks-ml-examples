1d1982b5fad2e8a50060fe99326cb42e445492cb
Wenfei Yan <87323464+wenfeiy-db@users.noreply.github.com>
2023-10-18 17:36:14 -0700

    Add optimize metadata for llama2-7b (#82)
    * init
    
    * optimize llm + input example work for both
    
    * add prompt in ai gateway
    
    * Revert "init"
    
    This reverts commit cdd357127b4818b9506b8636f12202310758225b.
    
    * clean up 02
    
    * update
    
    ---------
    
    Co-authored-by: wenfeiy-db <wenfeiy-db>

--- llm-models/llamav2/llamav2-7b/02_mlflow_logging_inference.py
+++ llm-models/llamav2/llamav2-7b/02_mlflow_logging_inference.py
@@ -13,7 +13,7 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install --upgrade "mlflow-skinny[databricks]>=2.4.1"
+# MAGIC %pip install --upgrade "mlflow-skinny[databricks]>=2.6.0"
 # MAGIC dbutils.library.restartPython()
 
 # COMMAND ----------
@@ -30,20 +30,18 @@ notebook_login()
 
 # COMMAND ----------
 
-# it is suggested to pin the revision commit hash and not change it for reproducibility because the uploader might change the model afterwards; you can find the commmit history of llamav2-7b-chat in https://huggingface.co/meta-llama/Llama-2-7b-chat-hf/commits/main
-model = "meta-llama/Llama-2-7b-chat-hf"
+# It is suggested to pin the revision commit hash and not change it for reproducibility because the uploader might change the model afterwards; you can find the commmit history of llamav2-7b-chat in https://huggingface.co/meta-llama/Llama-2-7b-chat-hf/commits/main
+model_name = "meta-llama/Llama-2-7b-chat-hf"
 revision = "08751db2aca9bf2f7f80d2e516117a53d7450235"
 
-from huggingface_hub import snapshot_download
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-# If the model has been downloaded in previous cells, this will not repetitively download large model files, but only the remaining files in the repo
-snapshot_location = snapshot_download(repo_id=model, revision=revision, ignore_patterns="*.safetensors*")
+# Load model
+model = AutoModelForCausalLM.from_pretrained(model_name, revision=revision, torch_dtype=torch.bfloat16, cache_dir="/local_disk0/.cache/huggingface/")
+tokenizer = AutoTokenizer.from_pretrained(model_name, revision=revision)
 
 # COMMAND ----------
-
-import mlflow
-import torch
-import transformers
 
 # Define prompt template to get the expected features and performance for the chat versions. See our reference code in github for details: https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L212
 
@@ -52,101 +50,48 @@ You are a helpful, respectful and honest assistant. Always answer as helpfully a
 
 If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
 
-# Define PythonModel to log with mlflow.pyfunc.log_model
-
-class Llama2(mlflow.pyfunc.PythonModel):
-    def load_context(self, context):
-        """
-        This method initializes the tokenizer and language model
-        using the specified model repository.
-        """
-        # Initialize tokenizer and language model
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
-            context.artifacts['repository'], padding_side="left")
-        self.model = transformers.AutoModelForCausalLM.from_pretrained(
-            context.artifacts['repository'], 
-            torch_dtype=torch.bfloat16,
-            low_cpu_mem_usage=True, 
-            trust_remote_code=True,
-            device_map="auto",
-            pad_token_id=self.tokenizer.eos_token_id)
-        self.model.eval()
-
-    def _build_prompt(self, instruction):
-        """
-        This method generates the prompt for the model.
-        """
-        return f"""<s>[INST]<<SYS>>\n{DEFAULT_SYSTEM_PROMPT}\n<</SYS>>\n\n\n{instruction}[/INST]\n"""
-
-    def _generate_response(self, prompt, temperature, max_new_tokens):
-        """
-        This method generates prediction for a single input.
-        """
-        # Build the prompt
-        prompt = self._build_prompt(prompt)
-
-        # Encode the input and generate prediction
-        encoded_input = self.tokenizer.encode(prompt, return_tensors='pt').to('cuda')
-        output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_new_tokens=max_new_tokens)
-    
-        # Decode the prediction to text
-        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
-
-        # Removing the prompt from the generated text
-        prompt_length = len(self.tokenizer.encode(prompt, return_tensors='pt')[0])
-        generated_response = self.tokenizer.decode(output[0][prompt_length:], skip_special_tokens=True)
-
-        return generated_response
-      
-    def predict(self, context, model_input):
-        """
-        This method generates prediction for the given input.
-        """
-
-        outputs = []
-
-        for i in range(len(model_input)):
-          prompt = model_input["prompt"][i]
-          temperature = model_input.get("temperature", [1.0])[i]
-          max_new_tokens = model_input.get("max_new_tokens", [100])[i]
-
-          outputs.append(self._generate_response(prompt, temperature, max_new_tokens))
-      
-        # {"candidates": [...]} is the required response format for MLflow AI gateway -- see 07_ai_gateway for example
-        return {"candidates": outputs}
+def build_prompt(instruction):
+    return f"""<s>[INST]<<SYS>>\n{DEFAULT_SYSTEM_PROMPT}\n<</SYS>>\n\n\n{instruction}[/INST]\n"""
 
 # COMMAND ----------
 
+import mlflow
+from mlflow.models import infer_signature
 from mlflow.models.signature import ModelSignature
 from mlflow.types import DataType, Schema, ColSpec
 
 import pandas as pd
 
-# Define input and output schema
-input_schema = Schema([
-    ColSpec(DataType.string, "prompt"), 
-    ColSpec(DataType.double, "temperature", optional=True), 
-    ColSpec(DataType.long, "max_new_tokens", optional=True)])
-output_schema = Schema([ColSpec(DataType.string)])
-signature = ModelSignature(inputs=input_schema, outputs=output_schema)
 
-# Define input example
-input_example=pd.DataFrame({
-            "prompt":["what is ML?"], 
-            "temperature": [0.5],
-            "max_new_tokens": [100]})
+# Define model signature including params
+input_example = {"prompt": build_prompt("What is Machine Learning?")}
+inference_config = {
+  "temperature": 1.0,
+  "max_new_tokens": 100,
+  "do_sample": True,
+}
+signature = infer_signature(
+  model_input=input_example,
+  model_output="Machien Learning is...",
+  params=inference_config
+)
 
 # Log the model with its details such as artifacts, pip requirements and input example
 # This may take about 1.7 minutes to complete
 with mlflow.start_run() as run:  
-    mlflow.pyfunc.log_model(
-        "model",
-        python_model=Llama2(),
-        artifacts={'repository' : snapshot_location},
-        pip_requirements=["torch", "transformers", "accelerate", "sentencepiece"],
-        input_example=input_example,
-        signature=signature,
-    )
+  mlflow.transformers.log_model(
+    transformers_model={
+      "model": model,
+      "tokenizer": tokenizer,
+    },
+    task = "text-generation",
+    artifact_path="model",
+    pip_requirements=["torch", "transformers", "accelerate", "sentencepiece"],
+    input_example=input_example,
+    signature=signature,
+    # Add the metadata task so that the model serving endpoint created later will be optimized
+    metadata={"task": "llm/v1/completions"}
+  )
 
 # COMMAND ----------
 
@@ -169,7 +114,6 @@ mlflow.set_registry_uri("databricks-uc")
 # This may take 2.2 minutes to complete
 
 registered_name = "models.default.llama2_7b_completions" # Note that the UC model name follows the pattern <catalog_name>.<schema_name>.<model_name>, corresponding to the catalog, schema, and registered model name
-
 
 result = mlflow.register_model(
     "runs:/"+run.info.run_id+"/model",
@@ -194,24 +138,28 @@ client.set_registered_model_alias(name=registered_name, alias="Champion", versio
 import mlflow
 import pandas as pd
 
+registered_name = "models.default.llama2_7b_completions_wenfei"
+
 loaded_model = mlflow.pyfunc.load_model(f"models:/{registered_name}@Champion")
 
 # Make a prediction using the loaded model
 loaded_model.predict(
-    {
-        "prompt": ["What is ML?", "What is large language model?"],
-        "temperature": [0.1, 0.5],
-        "max_new_tokens": [100, 100],
+  {"prompt": "What is large language model?"}, 
+  params={
+    "temperature": 0.5,
+    "max_new_tokens": 100,
     }
 )
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Create Model Serving Endpoint
+# MAGIC ## Create Optimized Model Serving Endpoint
 # MAGIC Once the model is registered, we can use API to create a Databricks GPU Model Serving Endpoint that serves the `LLaMAV2-7b` model.
 # MAGIC
-# MAGIC Note that the below deployment requires GPU model serving. For more information on GPU model serving, contact the Databricks team or sign up [here](https://docs.google.com/forms/d/1-GWIlfjlIaclqDz6BPODI2j1Xg4f4WbFvBXyebBpN-Y/edit).
+# MAGIC Note that the below deployment requires GPU model serving. For more information on GPU model serving, see the [documentation](https://docs.databricks.com/en/machine-learning/model-serving/create-manage-serving-endpoints.html#gpu). The feature is in Public Preview.
+# MAGIC
+# MAGIC Models in LLaMA-V2 family are supported for Optimized LLM Serving, which provides an order of magnitute better throughput and latency improvement. For more information, see the [documentation](https://docs.databricks.com/en/machine-learning/model-serving/llm-optimized-model-serving.html). In this section, the endpoint will have optimized LLM serving enabled by default. To disable it, remove the `metadata = {"task": "llm/v1/completions"}` when calling `log_model` and run the notebook again.
 
 # COMMAND ----------
 
